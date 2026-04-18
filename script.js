b373a962be10ae8fd7ee22b19c567e9d018d8fcc
parthasaradhyanil <25f2005855@ds.study.iitm.ac.in>
2026-04-18 08:18:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ function renderCalendar() {
             <div class="cal-cell">
                 <span style="font-size:0.75rem; color: lightgray; font-weight:bold;">${d}</span>
                 ${dayEvents.map(e => `
-                    href="${e.formLink}"  target="_blank" class="cal-ev-link  ${e.isFree ? '' : 'paid'}">
+                <a href="${e.formLink}"  target="_blank" class="cal-ev-link  ${e.isFree ? '' : 'paid'}">
                         ${e.title}
                     </a>
                 `).join('')}
